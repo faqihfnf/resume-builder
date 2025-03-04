000db7da48ec58b32feacf2642c6e0fc30de7b80
faqihfnf <faqih.fnf@gmail.com>
2025-03-04 08:37:05 +0700

    add dark mode

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,11 +1,14 @@
 "use client";
+import ThemeToggle from "@/components/ThemeToggle";
 import { UserButton } from "@clerk/nextjs";
 import { CreditCardIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function Navbar() {
+  const { theme } = useTheme();
   return (
     <header className="shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-3 p-3">
@@ -21,21 +24,25 @@ export default function Navbar() {
             Resume Builder
           </span>
         </Link>
-        <UserButton
-          appearance={{
-            elements: {
-              avatarBox: "h-35 w-35",
-            },
-          }}
-        >
-          <UserButton.MenuItems>
-            <UserButton.Link
-              label="Billing"
-              labelIcon={<CreditCardIcon className="h-4 w-4" />}
-              href="/billing"
-            />
-          </UserButton.MenuItems>
-        </UserButton>
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+          <UserButton
+            appearance={{
+              baseTheme: theme === "dark" ? dark : undefined,
+              elements: {
+                avatarBox: "h-35 w-35",
+              },
+            }}
+          >
+            <UserButton.MenuItems>
+              <UserButton.Link
+                label="Billing"
+                labelIcon={<CreditCardIcon className="h-4 w-4" />}
+                href="/billing"
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        </div>
       </div>
     </header>
   );
