398a605bee4be45c099cd3790fca40a9473143aa
faqihfnf <faqih.fnf@gmail.com>
2025-03-05 17:06:11 +0700

    add work experience into the breadcumbs

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -1,16 +1,26 @@
 import { EditorFormProps } from "@/lib/type";
 import GeneralInfoForm from "./forms/GeneralInfoForm";
 import PersonalInfoForm from "./forms/PersonalInfoForm";
+import WorkExperienceForm from "./forms/WorkExperienceForm";
 
 export const steps: {
   title: string;
   component: React.ComponentType<EditorFormProps>;
   key: string;
 }[] = [
-  { title: "General Info", component: GeneralInfoForm, key: "general-info" },
+  {
+    title: "General Info",
+    component: GeneralInfoForm,
+    key: "general-info",
+  },
   {
     title: "Personal Info",
     component: PersonalInfoForm,
     key: "personal-info",
+  },
+  {
+    title: "Work Experience",
+    component: WorkExperienceForm,
+    key: "work-experience",
   },
 ];
