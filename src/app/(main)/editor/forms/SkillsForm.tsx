89ce4d2e4f5c2d75441b3490782d3cc2f0b89578
faqihfnf <faqih.fnf@gmail.com>
2025-03-07 13:50:26 +0700

    add skills form into the breadcumbs

---
+++ src/app/(main)/editor/forms/SkillsForm.tsx
@@ -0,0 +1,79 @@
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { EditorFormProps } from "@/lib/type";
+import { skillsSchema, SkillsValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+export default function SkillsForm({
+  resumeData,
+  setResumeData,
+}: EditorFormProps) {
+  const form = useForm<SkillsValues>({
+    resolver: zodResolver(skillsSchema),
+    defaultValues: {
+      skills: resumeData.skills || [],
+    },
+  });
+
+  useEffect(() => {
+    const { unsubscribe } = form.watch(async (values) => {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+      setResumeData({
+        ...resumeData,
+        skills:
+          values.skills
+            ?.filter((skill) => skill !== undefined)
+            .map((skill) => skill.trim())
+            .filter((skill) => skill !== "") || [],
+      });
+    });
+    return unsubscribe;
+  }, [form, resumeData, setResumeData]);
+
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-3xl font-semibold">Skills</h2>
+        <p className="text-sm text-muted-foreground">
+          Add your skills to your resume
+        </p>
+      </div>
+      <Form {...form}>
+        <form className="space-y-3">
+          <FormField
+            control={form.control}
+            name="skills"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="sr-only">Skills</FormLabel>
+                <FormControl>
+                  <Textarea
+                    {...field}
+                    placeholder="e.g. HTML, CSS, JavaScript"
+                    onChange={(e) => {
+                      const skills = e.target.value.split(",");
+                      field.onChange(skills);
+                    }}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Separate each skill with a comma.
+                </FormDescription>
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
+}
