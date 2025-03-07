8d943f02128a45934615ad33b62f2a8d18b35968
faqihfnf <faqih.fnf@gmail.com>
2025-03-07 13:27:33 +0700

    add education form into the breadcumbs

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -2,6 +2,7 @@ import { EditorFormProps } from "@/lib/type";
 import GeneralInfoForm from "./forms/GeneralInfoForm";
 import PersonalInfoForm from "./forms/PersonalInfoForm";
 import WorkExperienceForm from "./forms/WorkExperienceForm";
+import EducationForm from "./forms/EducationForm";
 
 export const steps: {
   title: string;
@@ -22,5 +23,10 @@ export const steps: {
     title: "Work Experience",
     component: WorkExperienceForm,
     key: "work-experience",
+  },
+  {
+    title: "Education",
+    component: EducationForm,
+    key: "education",
   },
 ];
