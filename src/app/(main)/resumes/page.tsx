b9520a05077d3e6a5f7d7308a175a2c61104f182
faqihfnf <faqih.fnf@gmail.com>
2025-03-17 13:32:42 +0700

    update user can create resume if total resume less than 3

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -35,14 +35,18 @@ export default async function Page() {
     }),
   ]);
 
+  const canCreateNewResume = totalCount < 3;
+
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
-      <Button asChild className="mx-auto flex w-fit gap-2">
-        <Link href="/editor">
-          <PlusSquare className="size-5" />
-          New Resume
-        </Link>
-      </Button>
+      {canCreateNewResume && (
+        <Button asChild className="mx-auto flex w-fit gap-2">
+          <Link href="/editor">
+            <PlusSquare className="size-5" />
+            New Resume
+          </Link>
+        </Button>
+      )}
       <div className="space-y-1">
         <h1 className="text-3xl font-bold">Your Resume</h1>
         <p className="font-semibold">Total Resume : {totalCount}</p>
